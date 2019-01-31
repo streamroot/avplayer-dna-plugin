44ab7254cbac5628cbdd9afe26c8f43eed63e11c
LamineNdy <lamine.ndiaye@streamroot.io>
2019-01-31 19:20:18 +0100

    # This is a combination of 2 commits. # This is the 1st commit message:
    Build workspace from pod update
    
    # The commit message #2 will be skipped:
    
    # Updat eplugin to SDK 3.9.0

---
+++ PlayerDNAPlugin/AVPlayerDNAPlugin.h
@@ -0,0 +1,19 @@
+//
+//  AVPlayerDNAPlugin.h
+//  AVPlayerDNAPlugin
+//
+//  Created by Lamine Ndiaye on 10/09/2018.
+//  Copyright © 2018 Streamroot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AVPlayerDNAPlugin.
+FOUNDATION_EXPORT double AVPlayerDNAPluginVersionNumber;
+
+//! Project version string for AVPlayerDNAPlugin.
+FOUNDATION_EXPORT const unsigned char AVPlayerDNAPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVPlayerDNAPlugin/PublicHeader.h>
+
+
