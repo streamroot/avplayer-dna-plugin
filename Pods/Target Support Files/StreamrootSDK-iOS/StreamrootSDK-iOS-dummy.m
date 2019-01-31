180eafc00d14035789eb6544da4bc41ece9327e4
LamineNdy <lamine.ndiaye@streamroot.io>
2019-01-31 19:24:50 +0100

    Embed pods in the git repo

---
+++ Pods/Target Support Files/StreamrootSDK-iOS/StreamrootSDK-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_StreamrootSDK_iOS : NSObject
+@end
+@implementation PodsDummy_StreamrootSDK_iOS
+@end
---
+++ Pods/Target Support Files/StreamrootSDK-tvOS/StreamrootSDK-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_StreamrootSDK_tvOS : NSObject
+@end
+@implementation PodsDummy_StreamrootSDK_tvOS
+@end
